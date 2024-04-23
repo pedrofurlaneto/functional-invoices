34b551f48bffdea4500c081bcf21615f6a47f5de
1canas <pedro.s.furlaneto@gmail.com>
2024-04-23 16:36:21 -0300

    feat(takeUntil): implementation of takeUntil operatior for put a limit on function calling

--- public/app/services/nota-services.js
+++ public/app/services/nota-services.js
@@ -8,22 +8,22 @@ const filterItemsByCode = (code, items) => items.filter(item => item.codigo === 
 const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
-  list() {
-    return fetch(NOTAS_URL_API)
-        .then(handleStatus)
-        .catch(err => {
-            console.log(err);
+    list() {
+        return fetch(NOTAS_URL_API)
+            .then(handleStatus)
+            .catch(err => {
+                console.log(err);
 
-            return Promise.reject('Não foi possível obter as notas fiscais');
-        });
-  },
-  sumItems(code) {
-    const filterItems = partialize(filterItemsByCode, code);
-    const sumItems = pipe(
-        getItemsFromNotas,
-        filterItems, 
-        sumItemsValue
-);
+                return Promise.reject('Não foi possível obter as notas fiscais');
+            });
+    },
+    sumItems(code) {
+        const filterItems = partialize(filterItemsByCode, code);
+        const sumItems = pipe(
+            getItemsFromNotas,
+            filterItems, 
+            sumItemsValue
+        );
 
     return this.list()
         .then(sumItems)
