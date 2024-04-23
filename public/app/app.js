ed8c48eb848d778c65898e9ab66a994e2820788b
1canas <pedro.s.furlaneto@gmail.com>
2024-04-23 16:56:57 -0300

    feat(delay): implementation of delay operator

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,6 @@
-import { log } from './utils/promise-helpers.js'
+import { log, timeoutPromise, delay } from './utils/promise-helpers.js'
 import { notasService } from './services/nota-services.js'
-import { takeUntil, debounceTime, partialize } from './utils/operators.js'
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js'
 import './utils/array-helpers.js'
 
 const operations = pipe(
@@ -9,7 +9,8 @@ const operations = pipe(
 )
 
 const getSumInvoiceItems = operations(() => 
-    notasService.sumItems('2143')
+    timeoutPromise(200, notasService.sumItems('2143'))
+    .then(delay(5000))
     .then(log)
     .catch(log)
 );
--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -5,4 +5,21 @@ export const log = param => {
     console.log(param);
 
     return param;
+};
+
+export const timeoutPromise = (milliseconds, promise) => {
+    const timeout = new Promise((resolve, reject) => {
+        setTimeout(() => 
+            reject('Limite de tempo excedido! Tente novamente mais tarde.'),
+        milliseconds); 
+    });
+
+    return Promise.race([
+        timeout, promise
+    ]);
 }
+
+export const delay = milliseconds => data =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve(data), milliseconds);
+    });
