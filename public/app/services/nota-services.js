365c430abc590a0a3a03c14f5fdbdf366b094a61
1canas <pedro.s.furlaneto@gmail.com>
2024-04-23 15:48:32 -0300

    feat(partialize util): implementation of partialize operator for functions manipulating

--- public/app/services/nota-services.js
+++ public/app/services/nota-services.js
@@ -1,6 +1,11 @@
 import { handleStatus } from "../utils/promise-helpers.js";
+import { partialize } from "../utils/operators.js";
 
 const NOTAS_URL_API = "http://localhost:3000/notas";
+
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.item);
+const filterItemsByCode = (code, items) => items.filter(item => item.code === code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 const sumItems = (code) => (notas) =>
   notas
