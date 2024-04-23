415faafc19fe4024133516799178338de020171c
1canas <pedro.s.furlaneto@gmail.com>
2024-04-23 14:28:46 -0300

    feat(promise logger helper): incrementation of log helper function for promise

--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -2,5 +2,7 @@ export const handleStatus = res =>
     res.ok ? res.json() : Promise.reject(res.statusText);
 
 export const log = param => {
-    console.log(param)
+    console.log(param);
+
+    return param;
 }
