1fecf7a8ccc8784360f691b0e98a43f9ab7f4fd3
1canas <pedro.s.furlaneto@gmail.com>
2024-04-23 16:16:35 -0300

    feat(compose): implementation of compose functions operator

--- public/app/services/nota-services.js
+++ public/app/services/nota-services.js
@@ -1,23 +1,27 @@
-import { handleStatus } from "../utils/promise-helpers.js";
-import { partialize } from "../utils/operators.js";
+import { handleStatus, log } from "../utils/promise-helpers.js";
+import { partialize, compose } from "../utils/operators.js";
 
 const NOTAS_URL_API = "http://localhost:3000/notas";
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.item);
-const filterItemsByCode = (code, items) => items.filter(item => item.code === code);
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
 const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
-
-const sumItems = (code) => (notas) =>
-  notas
-    .$flatMap((nota) => nota.itens)
-    .filter((item) => item.codigo === code)
-    .reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
   list() {
-    return fetch(NOTAS_URL_API).then(handleStatus);
+    return fetch(NOTAS_URL_API)
+        .then(handleStatus)
+        .catch(err => {
+            console.log(err);
+
+            return Promise.reject('Não foi possível obter as notas fiscais');
+        });
   },
   sumItems(code) {
-    return this.list().then(sumItems(code));
-  },
+    const filterItems = partialize(filterItemsByCode, code);
+    const sumItems = compose(sumItemsValue, filterItems, getItemsFromNotas);
+
+    return this.list()
+        .then(sumItems)
+  }
 };
